fa2cddae2782f3ddb3ce63afd719d58da1b6567a
karolmartg <karolmartgamb@hotmail.com>
2025-04-16 00:14:40 -0600

    FirstCommit + Parte 1 + Parte 2

---
+++ CasoEstudioKN/CasoEstudioKN/Controllers/CasasController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasoEstudioKN.Controllers
+{
+    public class CasasController : Controller
+    {
+        #region Inicio
+        // GET: Casas
+        public ActionResult Index()
+        {
+            try
+            {
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+        }
+        #endregion
+
+        #region Consultar
+        [HttpGet]
+        public ActionResult Consultar()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+        }
+        #endregion
+
+        #region Alquilar
+        [HttpGet]
+        public ActionResult Alquilar()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ CasoEstudioKN/CasoEstudioKN/Models/CasasModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CasoEstudioKN.Models
+{
+    public class CasasModel
+    {
+        public string DescripcionCasa { get; set; }
+        public decimal PrecioCasa { get; set; }
+        public string UsuarioAlquiler { get; set; }
+        public DateTime FechaAlquiler { get; set; }
+    }
+}
